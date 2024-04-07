08a319d2c5588a491b178009e284a16c11c7c010
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-04-07 19:14:09 +0530

    rev

--- setup/config.sh
+++ setup/config.sh
@@ -1,12 +1,14 @@
-# paths
-this_dir=$(dirname $0)
-this_plugin_dir=run.nexplugin
+# temp
+script_dir=$(dirname $0)
+tpl_text=$(cat $script_dir/bashrc.tpl)
 root_path=$HOME/dotfiles
-nex_run_path=$root_path/$this_plugin_dir
+
+# paths
+this_plugin_name=run.nexplugin
+this_plugin_path=$root_path/$this_plugin_name
 
 # bashrc
 bashrc="$HOME/.bashrc"
 this_start_text='__nex_run_start'
 this_end_text='__nex_run_end'
-tpl_text=$(cat $this_dir/bashrc.tpl)
-this_middle_text="${tpl_text//__s1__/$nex_run_path}"
+this_middle_text="${tpl_text//__s1__/$this_plugin_path}"
