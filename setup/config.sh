ef96e77bdbac7712a4bf016611476b12336cc09e
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-04-07 14:38:55 +0530

    refac

--- setup/config.sh
+++ setup/config.sh
@@ -5,6 +5,7 @@ root_path=$HOME/dotfiles
 nex_run_path=$root_path/$this_plugin_dir
 
 # bashrc
+bashrc="$HOME/.bashrc"
 this_start_text='__nex_run_start'
 this_end_text='__nex_run_end'
 tpl_text=$(cat $this_dir/bashrc.tpl)
--- setup/install.sh
+++ setup/install.sh
@@ -3,14 +3,15 @@
 set -e
 
 source $(dirname "$0")/config.sh
-source $(dirname "$0")/lib.sh
+source $nex_support_path/src/plugin/install/helpers.sh
 
 main() {
-  local bashrc="$HOME/.bashrc"
   start
+  validate
   remove_from_bashrc
   copy_to_bashrc
   finish
 }
+
 
 main
